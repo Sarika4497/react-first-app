acca63853effa7d9f95bd59f7ecd215faffe8208
SARIKA PATIL <patilsarika2321@gmail.com>
2021-07-16 23:47:58 -0700

    added my first react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import Coding from './img/Coding.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My First React-app</h1>
+      <h2>Author: Sarika Patil</h2>
+      <img src={Coding} alt ="Coding "/>
     </div>
   );
 }
